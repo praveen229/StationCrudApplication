e31c00f15083605721e366965bd52936d651ce1a
c_pdivam <c_pdivam@qti.qualcomm.com>
2018-11-08 11:09:15 +0530

    initial commit

---
+++ src/main/java/com/example/StationCrudApp/StationCrudAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.StationCrudApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationCrudAppApplication.class, args);
+	}
+}
